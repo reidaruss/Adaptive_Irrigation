8ea4278909a61452d84e7c0db1efc05a0690f10d
reidaruss <reidarussell@gmail.com>
2021-05-11 19:58:24 -0500

    Added moisture logging and uploading to database.

--- Scheduler/Scheduler.py
+++ Scheduler/Scheduler.py
@@ -8,10 +8,15 @@
 # Imports
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import serial
+from datetime import datetime
+import pandas as pd
 
 # Global Variables
-connection_string = "mongodb+srv://readonly:readonly@[demodata.rgl39.mongodb.net/demo?retryWrites=true&w=majority](http://demodata.rgl39.mongodb.net/demo?retryWrites=true&w=majority)"
+connection_string = "mongodb://localhost:27017/"
 client = MongoClient(connection_string)
+db = client['sensor-data']
+collection = db['moisture']
 
 
 def get(post_id):
@@ -22,7 +27,10 @@ def get(post_id):
     # Send new data to database
 def update_db(message):
     print("Updating MongoDB...")
-
+    df = pd.DataFrame(message,columns=['Moisture','DT'])
+    print(df)
+    data = df.to_dict("records")
+    collection.insert_many(data)
 
 # Template for Mongo DB database
 # {
@@ -31,3 +39,95 @@ def update_db(message):
 #     moisture_value: '436',
 #     moisture_level: '85',
 # }
+
+
+class serialIO ():
+    def __init__(self, baud):
+        self.baud_rate = baud
+
+# Yields messages from serial port (generator)
+    def read_serial(self, serial_port):
+        ser = serial.Serial(port=serial_port, baudrate=self.baud_rate)
+        try:
+            ser.isOpen()
+            print("Serial Port is open.")
+        except:
+            print("Error opening serial port")
+            exit()
+
+        if ser.isOpen():
+            try:
+                while(1):
+                    yield(ser.readline())
+            except Exception:
+                print("Error reading serial port")
+        else:
+            print("Cannot open serial port")
+
+    def write_serial(self, serial_port, message):
+        ser = serial.Serial(port=serial_port, baudrate=self.baud_rate)
+        try:
+            ser.isOpen()
+            print("Serial Port is open.")
+        except:
+            print("Error opening serial port")
+            exit()
+
+        if ser.isOpen():
+            try:
+                while(1):
+                    ser.write(message)
+            except Exception:
+                print("Error writing to serial port")
+        else:
+            print("Cannot open serial port")
+
+def write_moisture_data(moisture):
+    f = open("waterlog_test_1.txt", "a")
+    for line in moisture:
+        f.write(line)
+
+def check_water_time():
+    now = datetime.now()
+    if now.minute % 2 == 0:
+        return True
+    else:
+        return False
+
+
+
+def start():
+    #serial_port = '/dev/tty.usbmodem144101'
+
+    serial_port = '/dev/tty.usbmodemL2000OAK1'
+    baud = 9600
+    # baud = 115200
+    sIO = serialIO(baud)
+    i = 0
+    moisture = []
+    big_list = []
+    for line in sIO.read_serial(serial_port):
+        # print(int.from_bytes(line[9:12],"big",signed="False"))
+        # print(str(line[9:12],'UTF-8'))
+        # moisture.append(line[9:12].decode("utf-8")+'\n')
+        moisture.append(str(line[9:12],'UTF-8'))
+        moisture.append(datetime.now())
+        big_list.append(moisture)
+        moisture = []
+        i += 1
+
+        # Experimental
+#        if check_water_time():
+#            sIO.write_serial("TTW:10")
+
+        # End Experimental
+
+
+
+        if i == 100:
+            # write_moisture_data(moisture)
+            update_db(big_list)
+            i = 0
+            big_list = []
+
+start()
